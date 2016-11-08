f7742fd883934268ac47e000938746daf9032265
Med <elasri.md@gmail.com>
2016-11-08 00:48:11 +0000

    Update bootstrap
    Export Debian Front-end for Ubuntu, add php/mysql repositories and Fix nginx symbolic link

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,25 +6,25 @@ MYSQL_ROOT_PASSWORD="root"
 sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password password \"'$MYSQL_ROOT_PASSWORD'\""
 sudo debconf-set-selections <<< "mysql-server-5.6 mysql-server/root_password_again password \"'$MYSQL_ROOT_PASSWORD'\""
 
+sudo apt-get update
 sudo apt-get install -y python-software-properties
 
 sudo add-apt-repository ppa:ondrej/php
 sudo add-apt-repository ppa:ondrej/mysql
 
 sudo apt-get update
-
-sudo apt-get install -y curl git build-essential
+sudo apt-get install -y vim curl git build-essential
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
 
-sudo apt-get install -y php7.0-fpm php7.0-cli php7.0-xml php7.0-mbstring nginx mysql-server-5.6
+sudo apt-get install -y nodejs php7.0-fpm php7.0-cli php7.0-xml php7.0-mbstring php7.0-zip nginx mysql-server-5.6
 
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 
 sudo rm -f /etc/nginx/sites-available/default
-sudo cp /provision/nginx/sites /etc/nginx/sites-available/
-sudo ln -s /etc/nginx/sites-available/nginx /etc/nginx/sites-enabled/
+sudo cp provision/nginx/sites/default.conf /etc/nginx/sites-available/
+sudo rm -rf provision/nginx/sites/default
+sudo ln -s /etc/nginx/sites-available/default.conf /etc/nginx/sites-enabled/
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo rm -rf /etc/nginx/sites-available/default
 
